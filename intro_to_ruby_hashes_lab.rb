569f993e2e188bf92a47e21950594b3395af6d6d
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-25 00:42:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
 	}
 end
 
-# #def monopoly_with_fourth_tier
+def monopoly_with_fourth_tier
 # 	monopoly = {:railroads => {
 # 	pieces: 4,   
 # 	names: {reading_railroad: {mortgage_value: "$100"},
@@ -52,4 +52,4 @@ end
 	  
 # 	}
 
-# end
+end
